97d5bb1944b20fc10a7a2848246aef25e20b8b5d
Ibrohim <ibroxim.rashidov9199@gmail.com>
2025-06-11 16:14:27 +0500

    Update language settings

--- apps/cauth/serializers/admin_serializer.py
+++ apps/cauth/serializers/admin_serializer.py
@@ -2,14 +2,18 @@
 
 from rest_framework import serializers
 from apps.cauth.models import User
+from apps.main.serializers.language_serializer import LanguageSerializer
+
+
 
 class AdminCreateUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
     email_verified = serializers.BooleanField(read_only=True, source='is_active')
+    language = LanguageSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'phone', 'role', 'email_verified', 'password' , 'email', 'is_active_user', 'total_tokens']
+        fields = ['id', 'username', 'first_name', 'last_name', 'phone', 'role', 'language', 'email_verified', 'password' , 'email', 'is_active_user', 'total_tokens']
 
     def create(self, validated_data):
         password = validated_data.pop('password')
--- apps/main/views/language_view.py
+++ apps/main/views/language_view.py
@@ -3,10 +3,18 @@
 from rest_framework import viewsets
 from apps.main.models import Language
 from apps.main.serializers import LanguageSerializer
-from rest_framework.permissions import IsAuthenticated
+from apps.cauth.permissions import AdminPermission
+from rest_framework.permissions import  AllowAny
 
 
 class LanguageViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AdminPermission]
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [AdminPermission]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
