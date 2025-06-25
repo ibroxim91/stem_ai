83f81ff6d9e0cab55ce9027c340e0afd1318f6b9
Ibrohim <ibroxim.rashidov9199@gmail.com>
2025-06-25 21:15:10 +0500

    Last updates

--- apps/bot/management/commands/run_bot.py
+++ apps/bot/management/commands/run_bot.py
@@ -13,15 +13,15 @@ load_dotenv()
 API_URL = os.getenv("API_URL", "http://localhost:8000")
 TOKEN = os.getenv("BOT_TOKEN") 
 
-class Command(BaseCommand):
-    help = "Telegram botni ishga tushiradi"
+# class Command(BaseCommand):
+#     help = "Telegram botni ishga tushiradi"
 
-    def handle(self, *args, **options):
-        asyncio.run(self.run_bot())
+#     def handle(self, *args, **options):
+#         asyncio.run(self.run_bot())
 
-    async def run_bot(self):
-        logging.basicConfig(level=logging.INFO)
-        bot = Bot(token=TOKEN)
-        dp = await setup_dispatcher()
-        # dp.include_router(main_router)
-        await dp.start_polling(bot)
+#     async def run_bot(self):
+#         logging.basicConfig(level=logging.INFO)
+#         bot = Bot(token=TOKEN)
+#         dp = await setup_dispatcher()
+#         # dp.include_router(main_router)
+#         await dp.start_polling(bot)
